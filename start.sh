be597b09a72c02118bf64e130f5053614c67a3cd
iagoDominguezCamean <72920871+iagoDominguezCamean@users.noreply.github.com>
2023-11-25 19:28:36 +0100

    usar LF en start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 python viajesrebollo/manage.py makemigrations
 python viajesrebollo/manage.py migrate
-python viajesrebollo/manage.py runserver
+python viajesrebollo/manage.py runserver 0.0.0.0:8000
