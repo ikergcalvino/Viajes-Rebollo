71aace7aa25ddc1b0edac864ff9fb2625dd8861d
iagoDominguezCamean <72920871+iagoDominguezCamean@users.noreply.github.com>
2023-11-25 20:36:50 +0100

    Añadidas posible urls necesarias

--- viajesrebollo/viajes/urls.py
+++ viajesrebollo/viajes/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create_package", views.create_package, name="createpackage"),
+    path("create_activity", views.create_activity, name="createactivity"),
+    path("create_trip_plan", views.create_trip_plan, name="createtripplan"),
 ]
--- viajesrebollo/viajes/views.py
+++ viajesrebollo/viajes/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Página de inicio")
+
+def create_package(request):
+    return HttpResponse("Crear paquete de viaje")
+
+def create_activity(request):
+    return HttpResponse("Crear nueva actividad")
+
+def create_trip_plan(request):
+    return HttpResponse("Crear plan de viaje")
