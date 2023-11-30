9ff787f5b8655422482bd8230813de9783fadad0
Iker García Calviño <iker.gcalvino@udc.es>
2023-11-30 12:33:32 +0000

    Actualización admin viajesrebollo

--- viajesrebollo/viajesrebollo/urls.py
+++ viajesrebollo/viajesrebollo/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from viajes import views
+
 urlpatterns = [
-    #path("", views.index, name="index"),
-    path('admin/', admin.site.urls),
     path('', include("viajes.urls")),
+    path('admin/', admin.site.urls),
 ]
